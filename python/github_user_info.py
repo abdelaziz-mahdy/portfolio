0f5daee583b809487f69c912ea07d9a9f0b98bed
Abdelaziz Mahdy <zezohassam@gmail.com>
2024-05-20 14:51:40 +0300

    graphql

--- python/github_user_info.py
+++ python/github_user_info.py
@@ -1,60 +1,99 @@
-import requests
 import os
-from github import Github
+import requests
+import json
 
-# Initialize GitHub API client
-token = os.getenv('GITHUB_TOKEN')
-g = Github(token)
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+HEADERS = {"Authorization": f"Bearer {GITHUB_TOKEN}"}
+
+def run_query(query):
+    request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=HEADERS)
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception(f"Query failed to run by returning code of {request.status_code}. {query}")
 
 def get_user_info(username):
-    user = g.get_user(username)
+    query = f"""
+    {{
+      user(login: "{username}") {{
+        login
+        avatarUrl
+        repositories(first: 100, isFork: false) {{
+          nodes {{
+            name
+            url
+            stargazerCount
+            object(expression: "HEAD:screenshot/") {{
+              ... on Tree {{
+                entries {{
+                  name
+                  object {{
+                    ... on Blob {{
+                      text
+                      url
+                    }}
+                  }}
+                }}
+              }}
+            }}
+            hasPages
+            homepageUrl
+          }}
+        }}
+        pullRequests(first: 100) {{
+          nodes {{
+            title
+            url
+            baseRepository {{
+              nameWithOwner
+              stargazerCount
+              url
+            }}
+          }}
+        }}
+      }}
+    }}
+    """
+    result = run_query(query)
+    user_data = result['data']['user']
+
     user_info = {
-        'username': user.login,
-        'image_url': user.avatar_url,
+        'username': user_data['login'],
+        'image_url': user_data['avatarUrl'],
         'repos': [],
         'pull_requests': []
     }
 
-    # Get all repos that are not forks
-    for repo in user.get_repos():
-        if not repo.fork:
-            repo_info = {
-                'name': repo.name,
-                'stars': repo.stargazers_count,
-                'link': repo.html_url,
-                'image': False,
-                'image_link': None,
-                'github_pages_link': None
-            }
+    # Process repositories
+    for repo in user_data['repositories']['nodes']:
+        repo_info = {
+            'name': repo['name'],
+            'stars': repo['stargazerCount'],
+            'link': repo['url'],
+            'image': False,
+            'image_link': None,
+            'github_pages_link': repo['homepageUrl'] if repo['hasPages'] else None
+        }
 
-            # Check for screenshot/image in repo
-            contents = repo.get_contents("")
-            for content_file in contents:
-                if 'screenshot' in content_file.path.lower() or 'image' in content_file.path.lower():
+        if repo['object']:
+            for entry in repo['object']['entries']:
+                if 'screenshot' in entry['name'].lower() or 'image' in entry['name'].lower():
                     repo_info['image'] = True
-                    repo_info['image_link'] = content_file.download_url
+                    repo_info['image_link'] = entry['object']['url']
                     break
 
-            # Check for GitHub Pages
-            if repo.has_pages:
-                repo_info['github_pages_link'] = f'https://{username}.github.io/{repo.name}/'
+        user_info['repos'].append(repo_info)
 
-            user_info['repos'].append(repo_info)
-
-    # Get all pull requests and the stars of the main repo
-    for repo in user.get_repos():
-        prs = repo.get_pulls(state='all')
-        for pr in prs:
-            if pr.user.login == username:
-                base_repo = pr.base.repo
-                pull_request_info = {
-                    'title': pr.title,
-                    'link': pr.html_url,
-                    'base_repo_name': base_repo.full_name,
-                    'base_repo_stars': base_repo.stargazers_count,
-                    'base_repo_link': base_repo.html_url
-                }
-                user_info['pull_requests'].append(pull_request_info)
+    # Process pull requests
+    for pr in user_data['pullRequests']['nodes']:
+        pull_request_info = {
+            'title': pr['title'],
+            'link': pr['url'],
+            'base_repo_name': pr['baseRepository']['nameWithOwner'],
+            'base_repo_stars': pr['baseRepository']['stargazerCount'],
+            'base_repo_link': pr['baseRepository']['url']
+        }
+        user_info['pull_requests'].append(pull_request_info)
 
     return user_info
 
@@ -63,5 +102,5 @@ if __name__ == '__main__':
     owner, _ = repo_name.split('/')
     user_info = get_user_info(owner)
 
-    # Print user info for debugging purposes
-    print(user_info)
+    # Print user info in JSON format for cleaner readability
+    print(json.dumps(user_info, indent=4))
